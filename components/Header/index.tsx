1b016d25db0f526c801c101511f31f04adadeb78
Taylor Cottrell <32750540+tlcottr@users.noreply.github.com>
2023-03-17 17:46:38 -0400

    fixed header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -16,20 +16,22 @@ export default function Header(props: { showContent: boolean }) {
   };
 
   return (
-    <div
-      className={`flex flex-row w-full items-center justify-between border-b border-solid border-white pt-6 pb-4 z-[101] ${
-        showContent
-          ? "opacity-100 transition-opacity duration-1000 ease-in"
-          : "opacity-0"
-      }`}
-    >
-      {gameState === "home" ? (
-        <Symbol />
-      ) : (
-        <div onClick={handleClick}>
-          <RightArrow />
-        </div>
-      )}
+    <div className="fixed top-0 left-0 right-0 bg-black z-[101]">
+      <div
+        className={`flex flex-row w-full items-center justify-between border-b border-solid border-white pt-6 pb-4 px-4 ${
+          showContent
+            ? "opacity-100 transition-opacity duration-1000 ease-in"
+            : "opacity-0"
+        }`}
+      >
+        {gameState === "home" ? (
+          <Symbol />
+        ) : (
+          <div onClick={handleClick}>
+            <RightArrow />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
