57a997fd05313f79c50239d70b1e80e3011af66c
Taylor Cottrell <32750540+tlcottr@users.noreply.github.com>
2023-03-18 16:21:48 -0400

    update

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -16,7 +16,7 @@ export default function Layout(props: { children: any }) {
   return (
     <div className="flex flex-col min-h-screen relative mx-4 m-auto fixed top-0 bottom-0 overflow-hidden">
       <Header showContent={showContent} />
-      <main className="flex-1 flex flex-col fixed top-0 bottom-0 left-0 right-0 overflow-hidden">{children}</main>
+      <main className="flex-1 flex flex-col fixed top-0 bottom-0 left-3 right-0 overflow-hidden">{children}</main>
     </div>
   );
 }
