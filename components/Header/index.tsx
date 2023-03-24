7e4dc91d10de2120be8eb75d2dc37e9e3108ed32
Taylor Cottrell <32750540+tlcottr@users.noreply.github.com>
2023-03-24 10:46:23 -0400

    update

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -18,11 +18,11 @@ export default function Header(props: { showContent: boolean }) {
   return (
     <div
       className={`${
-        gameState === "gameplay" ? "z-1" : "z-[101]"
-      } fixed top-0 left-0 right-0 bg-black`}
+        gameState === "gameplay" ? "bg-none" : "bg-black"
+      } fixed top-0 left-0 right-0 border-b border-solid border-white z-[101]`}
     >
       <div
-        className={`flex flex-row w-full items-center justify-between border-b border-solid border-white pt-6 pb-4 px-4 ${
+        className={`flex flex-row w-full items-center justify-between pt-6 pb-4 px-4 ${
           showContent
             ? "opacity-100 transition-opacity duration-1000 ease-in"
             : "opacity-0"
