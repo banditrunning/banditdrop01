7c7e129d4f28a7233fa123c85690667878e83397
Taylor Cottrell <32750540+tlcottr@users.noreply.github.com>
2023-03-29 08:44:55 -0400

    add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 import Layout from "@/components/Layout";
 import GameContextProvider from "@/components/GameContextProvider";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <GameContextProvider>
       <Layout>
         <Component {...pageProps} />;
+        <Analytics />
       </Layout>
     </GameContextProvider>
   );
